6ef0b7083166181ce73472ec894805f6aa41dcf6
Favian Naufal Al Farrel <alfarrel0208@gmail.com>
2026-09-02 09:34:10 +0700

    Update Segitiga12.java

--- DasPro/Project/Jobsheet2/Segitiga12.java
+++ DasPro/Project/Jobsheet2/Segitiga12.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Segitiga12 {
-    public static void main(String[] args) {
-    
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan alas segitiga: ");
+        int alas = scanner.nextInt();
+
+        System.out.print("Masukkan tinggi segitiga: ");
+        int tinggi = scanner.nextInt();
+
+        double luasSegitiga = 0.5 * alas * tinggi;
+        System.out.println("Luas Segitiga: " + luasSegitiga);
     }
 }
